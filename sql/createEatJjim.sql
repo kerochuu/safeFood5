06dccbd1b1fb1025224139de54c97067a93e464d
김준호 <kerochuuu@gmail.com>
2019-05-15 16:55:29 +0900

    sql plus

---
+++ sql/createEatJjim.sql
@@ -0,0 +1,22 @@
+USE `safefood` ;
+CREATE TABLE IF NOT EXISTS `safefood`.`eat` (
+  `eat_code` INT(11) NOT NULL AUTO_INCREMENT,
+  `eat_user_code` INT(11) NULL DEFAULT NULL,
+  `eat_food_code` INT(11) DEFAULT NULL,
+  `eat_food_name` VARCHAR(200) NULL DEFAULT NULL,
+  
+  PRIMARY KEY (`eat_code`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `safefood`.`jjim` (
+  `jjim_code` INT(11) NOT NULL AUTO_INCREMENT,
+  `jjim_user_code` INT(11) NULL DEFAULT NULL,
+  `jjim_food_code` INT(11) DEFAULT NULL,
+  `jjim_food_name` VARCHAR(200) NULL DEFAULT NULL,
+  PRIMARY KEY (`jjim_code`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = utf8;
+
