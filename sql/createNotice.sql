fe363b7d77c9369f36a8d5cad2c922c04ca29171
소하연 <navy2629@naver.com>
2019-05-15 17:12:01 +0900

    createNotice.sql

--- sql/createNotice.sql
+++ sql/createNotice.sql
@@ -13,25 +13,6 @@ ENGINE = InnoDB
 AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = utf8;
 
-USE `safefood` ;
-CREATE TABLE IF NOT EXISTS `safefood`.`eat` (
-  `eat_code` INT(11) NOT NULL AUTO_INCREMENT,
-  `eat_user_code` INT(11) NULL DEFAULT NULL,
-  `eat_food_code` INT(11) DEFAULT NULL,
-  PRIMARY KEY (`eat_code`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8;
-
-CREATE TABLE IF NOT EXISTS `safefood`.`like` (
-  `like_code` INT(11) NOT NULL AUTO_INCREMENT,
-  `like_user_code` INT(11) NULL DEFAULT NULL,
-  `like_food_code` INT(11) DEFAULT NULL,
-  PRIMARY KEY (`eat_code`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8;
-
 
 ALTER TABLE `safefood`.`user` 
 ADD COLUMN `list` VARCHAR(400) NULL AFTER `user_likelist`;
